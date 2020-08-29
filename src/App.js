0d16047e85e5172096df2da3a6e9aba84fca8b38
Colin <colin.diesh@gmail.com>
2020-08-29 04:43:32 -0400

    React art performance

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import "./App.css";
+import { Surface } from "react-art";
+import Rectangle from "react-art/Rectangle";
+
+const N = 1000;
+const posx = [...Array(N)].map((e) => Math.random() * 1000);
+const posy = [...Array(N)].map((e) => Math.random() * 1600 - 800);
+const sizex = [...Array(N)].map((e) => Math.random() * 50);
+const sizey = [...Array(N)].map((e) => Math.random() * 50);
 
 function App() {
+  const ref = useRef();
+  const [offset, setOffset] = useState(0);
+  const scheduled = useRef(false);
+  const deltaY = useRef(0);
+  useEffect(() => {
+    const curr = ref.current;
+    if (curr) {
+      function onWheel(event) {
+        event.preventDefault();
+        if (!scheduled.current) {
+          scheduled.current = true;
+          deltaY.current += event.deltaY;
+          window.requestAnimationFrame(() => {
+            setOffset((offset) => offset + deltaY.current);
+            scheduled.current = false;
+            deltaY.current = 0;
+          });
+        }
+      }
+      curr.addEventListener("wheel", onWheel);
+      return () => curr.removeEventListener("wheel", onWheel);
+    }
+  }, [offset]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={ref} className="App">
+      <Surface width={1000} height={500}>
+        {new Array(N).fill(0).map((elt, index) => {
+          return (
+            <Rectangle
+              key={index}
+              x={posx[index]}
+              y={posy[index] + offset}
+              width={sizex[index]}
+              height={sizey[index]}
+              fill="red"
+            />
+          );
+        })}
+      </Surface>
     </div>
   );
 }
